e904feb6d8ab687961e43eaacef10d7022f64b59
kangyu <kangyu@139.com>
2018-03-26 11:12:45 +0800

    Rowmapp通过接口完成了代码的抽取

--- src/cn/yd/shop/dao/BaseDaoImpl.java
+++ src/cn/yd/shop/dao/BaseDaoImpl.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import cn.yd.shop.util.JdbcUtil;
